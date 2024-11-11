08d55d512875b600220299fba9698f5344bfd7f3
C354RCL <cesarcornejolopez10@gmail.com>
2024-11-11 10:35:03 -0600

    Modificaciones a navbar

--- src/Pages/Graficas.jsx
+++ src/Pages/Graficas.jsx
@@ -1,18 +1,23 @@
 import BarChart from './components/BarChart.jsx';
 
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-const cantidadPorHoras = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
-const cantidadPorNoches = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
 
 function App() {
+  const cantidadHoras = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+  const cantidadNoches = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+  try {
+
+  } catch (err) {
+    console.error("Error: ", err);
+  }
   return (
     <div className='flex flex-col items-center justify-center h-full p-10'>
       <h1>Gráfico de Ventas</h1>
       <div className='w-1/2 h-1/2 rounded-lg flex justify-center'>
-        <BarChart labels={meses} datos={cantidadPorHoras} title='Servicos por horas' />
+        <BarChart labels={meses} datos={cantidadHoras} title='Servicos por horas' />
       </div>
       <div className='w-1/2 h-1/2 rounded-lg flex justify-center'>
-        <BarChart labels={meses} datos={cantidadPorNoches} title='Servicios por noches' color='rgba(188, 226, 229, 1)'/>
+        <BarChart labels={meses} datos={cantidadNoches} title='Servicios por noches' color='rgba(188, 226, 229, 1)'/>
       </div>
     </div>
   );
